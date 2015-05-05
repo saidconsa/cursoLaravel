82584f27e6648bbc016d3c5c532a422a15d4ee7b
Said Alarcon Sosa <saidconsa@gmail.com>
2015-05-05 02:39:03 -0500

    Separando lógica y presentación
    Se crea un archivo para trabajar únicamente PHP en dicho archivo y el
    otro solo se escribe código HTML al cual se le incluye el archivo

--- view.php
+++ view.php
@@ -3,10 +3,16 @@
 	* // Said Alarcon Sosa																	*
 	* // T.S.U. en Tecnologias de la Informacion y Comunicacion, Area Sistemas Informaticos	*
 	* // saidconsa@gmail.com																*
-	* // Fecha: 05/05/2015 02:21:16															*
+	* // Fecha: 05/05/2015 02:24:23															*
 	****************************************************************************************/
-
-	$lenguaje = "PHP";
-	
-	require "view.php";
 ?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=Cp1252">
+<title>Separando Logica y Presentacion</title>
+</head>
+<body>
+	Aprendiendo <strong><?= $lenguaje;?></strong>
+</body>
+</html>
