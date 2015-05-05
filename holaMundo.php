d102c305acd1185dea7acf476198b0d4221af60a
Said Alarcon Sosa <saidconsa@gmail.com>
2015-05-05 02:17:30 -0500

    Iniciando Curso Laravel
    Se genera el proyecto para el primer Hola Mundo

---
+++ holaMundo.php
@@ -0,0 +1,10 @@
+<?php
+	/****************************************************************************************
+	* // Said Alarcon Sosa																	*
+	* // T.S.U. en Tecnologias de la Informacion y Comunicacion, Area Sistemas Informaticos	*
+	* // saidconsa@gmail.com																*
+	* // Fecha: 05/05/2015 02:14:31																	*
+	****************************************************************************************/
+	
+	echo "Hola Mundo ! ! !";
+?>
